ecedd27f5ad19f690fa7d5856c02b1710c36feec
Flo-Think <florian_knoop@gmx.de>
2019-07-13 04:02:43 +0200

    update progressbar

--- progressbar/selfmade/progressbar.py
+++ progressbar/selfmade/progressbar.py
@@ -4,25 +4,38 @@ import sys
 
 
 def progressbar(it, prefix="progress:", size=35, file=sys.stdout):
-    count = max(1, len(it))
-    n = len(str(count)) 
+    """a simple progress bar to decorate an iterator
 
-    def show(j):
-        x = int(size * j / count)
-        counter = "{:{}d}/{}".format(j, n, count)
+    Args:
+        it (iterator): show progressbar for this iterator
+        prefix (str): prefix for the progressbar
+        size (int): size of the progress bar
+        file (file): file to write to
+    """
+    count = max(1, len(it))
+    n = len(str(count)) + 1
+
+    def show(jj):
+        """show the progressbar"""
+        x = int(size * jj / count)
+        counter = "{:{}d}/{}".format(jj, n, count)
         bar = "{} |{}{}| {}\r".format(prefix, "|" * x, " " * (size - x), counter)
-        # bar = "{} {}{} {}\r".format(prefix, "|" * x, "." * (size - x), counter)
         file.write(bar)
         file.flush()
 
     show(0)
 
-    for i, item in enumerate(it):
+    ii = 0
+    for ii, item in enumerate(it):
         yield item
-        show(i + 1)
+        if file.isatty():
+            show(ii + 1)
 
-    file.write("\n")
-    file.flush()
+    if file.isatty():
+        file.write("\n")
+        file.flush()
+    else:
+        show(ii + 1)
 
 
 if __name__ == "__main__":
@@ -30,5 +43,5 @@ if __name__ == "__main__":
 
     # for _ in progressbar(range(15), "Computing: ", 30):
     for _ in progressbar(range(15)):  # , "Computing: ", 30):
-        time.sleep(0.2)  # any calculation you need
+        time.sleep(0.1)  # any calculation you need
 
