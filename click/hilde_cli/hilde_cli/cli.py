a9e3a201ec01cbda8786d047076c67ec9b74d4fc
Florian Knoop <knoop@fhi-berlin.mpg.de>
2019-07-17 10:50:18 +0200

    click_completion

--- click/hilde_cli/hilde_cli/cli.py
+++ click/hilde_cli/hilde_cli/cli.py
@@ -2,43 +2,48 @@ from pathlib import Path
 import importlib.resources as pkg_resources
 
 import click
+import click_completion
 import attr
 
 INFO_POSSIBILITES = ["geometry", "settings", "trajectory"]
 WORKFLOW_POSSIBILITES = ["aims", "phonopy", "md"]
 
 
+def complete(self, ctx, incomplete):
+
+    if incomplete.startswith("/"):
+        path_orig = incomplete
+    else:
+        path_orig = os.path.join(os.getcwd(), incomplete)
+
+    path, file_prefix = path_orig.rsplit("/", 1)
+
+    result = []
+    if not path:
+        path = "/"
+
+    for filename in os.listdir(path):
+
+        if not file_prefix or filename.startswith(file_prefix):
+
+            file_path = os.path.join(path, filename)
+            if os.path.isdir(file_path):
+                filename += "/"
+
+            if incomplete.startswith("/"):
+                result.append(os.path.join(path, filename))
+            else:
+                result.append(filename)
+
+    return result
+
+
+click_completion.init(match_incomplete=complete)
+
+
 @attr.s
 class CliTracker:
     input_filename = attr.ib(default=None)
-    geometry_filename = attr.ib(default="geometry.in")
-    settings_filename = attr.ib(default="settings.in")
-    trajectory_filename = attr.ib(default="trajectory.son")
-    config_filename = attr.ib(default="~/.hilderc")
-    workdir = attr.ib(default="aims_calculation")
-
-    info_possibilites = attr.ib(default=INFO_POSSIBILITES)
-    workflow_possibilites = attr.ib(default=WORKFLOW_POSSIBILITES)
-
-    full_input = attr.ib(default=False)
-
-    @property
-    def geometry_file(self):
-        return Path(self.geometry_filename)
-
-    @geometry_file.setter
-    def geometry_file(self, new_file):
-        self.check_path(new_file)
-        self.geometry_filename = new_file
-
-    @property
-    def settings_file(self):
-        return Path(self.settings_filename)
-
-    @settings_file.setter
-    def settings_file(self, new_file):
-        self.check_path(new_file)
-        self.settings_filename = new_file
 
     @property
     def input_file(self):
@@ -63,82 +68,11 @@ def cli(ctx):
     ctx.help_option_names = ["-h", "--help"]
 
 
-@cli.group()
-def info():
-    """inform about content of a file"""
-    pass
-
-
-@info.command("geometry")
-@click.argument("filename")
+@cli.command("info")
+@click.argument("filename", type=click.Path(exists=True))
 @click.pass_obj
-def geometry_info(obj, filename):
+def info(obj, filename):
     """inform about content of a geometry file"""
 
-    obj.geometry_file = filename
-    click.echo(obj.geometry_file.read_text())
-
-
-@info.command("settings")
-@click.argument("filename", default="settings.in")
-@click.pass_obj
-def settings_info(obj, filename):
-    """inform about content of a settings.in file"""
-
-    obj.settings_file = filename
-    click.echo(obj.settings_file.read_text())
-
-
-@cli.group()
-@click.option("--full", is_flag=True, help="list all options", show_default=True)
-@click.pass_obj
-def input(obj, full):
-    """provide template settings.in for workflows"""
-    obj.full_input = full
-
-
-@input.command("aims")
-@click.argument("filename", default="settings.in")
-@click.option("--allow_overwrite", is_flag=True)
-@click.pass_obj
-def aims_input(obj, filename, allow_overwrite):
-    """provide template settings.in for WORKFLOW.
-
-    Use `hilde input list` to list all possibilites."""
-
-    from .templates import settings
-
-    if obj.full_input:
-        template_file = "aims_full"
-    else:
-        template_file = "aims"
-
-    template = pkg_resources.read_text(settings, template_file)
-
-    outfile = Path(filename)
-
-    if not allow_overwrite and outfile.exists():
-        msg = f"{outfile} exists."
-        raise click.ClickException(msg)
-
-    outfile.write_text(template)
-
-    click.echo(f"Default {template_file} settings file written to {filename}.")
-
-
-@cli.group()
-@click.option("--workdir", help="provide a working directory to run the calculation")
-@click.option("--settings", default="settings.in", show_default=True)
-@click.pass_obj
-def run(obj, workdir, settings):
-    """run a hilde workflow"""
-    obj.workdir = workdir
-    obj.settings_file = settings
-
-
-@run.command("aims")
-@click.pass_obj
-def run_aims(obj):
-    """run and aims calculation"""
-
-    click.echo(f"run aims coming soon")
+    obj.input_file = filename
+    click.echo(obj.input_file.read_text())
